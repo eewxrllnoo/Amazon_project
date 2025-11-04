7f1b9b62d2f3ce8d3e7e1b5f5308e210c81544df
erwinllano <erwinllano95@gmail.com>
2025-11-05 06:43:41 +0800

    callbacks

--- data/products.js
+++ data/products.js
@@ -91,6 +91,33 @@ object3.method();
 // });
 // console.log(product1);
 
+export let products = [];
+
+export function loadProducts(renderProductsGrid) {
+  // Callback
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log("load products...");
+
+    renderProductsGrid();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+
+loadProducts();
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -568,3 +595,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
