0f7f32304981a38ce4babdeba5c288ca57647ddc
erwinllano <erwinllano95@gmail.com>
2025-11-04 19:36:37 +0800

    Send request to the backend

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -1,2 +1,8 @@
 const xhr = new XMLHttpRequest();
-xhr.open();
+
+xhr.addEventListener("load", () => {
+  console.log(xhr.response);
+});
+
+xhr.open("GET", "https://supersimplebackend.dev");
+xhr.send();
