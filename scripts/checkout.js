80a8020266d4e6136605642aebff5be7b96a5919
erwinllano <erwinllano95@gmail.com>
2025-11-16 23:18:48 +0800

    promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,16 @@ import { loadProducts } from "../data/products.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
+new Promise((resolve) => {
+  console.log("Start Promise");
+  loadProducts(() => {
+    console.log("finished loading");
+    resolve();
+  });
+}).then(() => {
+  console.log("Next step");
+});
+
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
