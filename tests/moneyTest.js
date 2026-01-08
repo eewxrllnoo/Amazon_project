20304d33698de4ea4d1d0f0e664b921449cd91dc
erwinllano <erwinllano95@gmail.com>
2026-01-09 04:30:39 +0800

    Create automated test for FormatCurrency

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,7 +1,28 @@
 import {formatCurrency} from "../scripts/utils/money.js";
 
+
+console.log('Test Suite: FormatCurrency Tests');
+
+console.log('Converts cents into dollars');
+
 if (formatCurrency(2095) === '20.95') {
     console.log('Test passed');
-}else {
+} else {
+    console.log('Test failed');
+}
+
+console.log('Works with zero');
+
+if (formatCurrency(0) === '0.00') {
+    console.log('Test passed');
+} else {
+    console.log('Test failed');
+}
+
+console.log('Rounds up to the nearest cent');
+
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('Test passed');
+} else {
     console.log('Test failed');
 }
