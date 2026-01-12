39e2400cff9ca4ca026750396da8ec92216054c6
erwinllano <erwinllano95@gmail.com>
2026-01-12 23:54:48 +0800

    Cartest jasmine testing framework

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,1 +1,12 @@
-  
+  import {addToCart, cart} from '../../data/cart.js';
+
+  describe('Test Suite: AddToCart Tests', () => {
+   it('Adds an existing product to the cart', () => {
+
+     })  
+     
+     it ('Adds a new product to the cart', () => {
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+     });
+   });
